cc636803826248291b665b5c701db0b59c90bed8
modmaker56 <walixrpcyt@gmail.com>
2024-06-14 13:54:51 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['254745936840']
-global.ownername = "KISH"//owner name
+global.ownernumber = ['923110692287']
+global.ownername = "ModMaker56"//owner name
 global.ytname = "YT: KishBotz.inc"
 global.socialm = "GitHub: Kish-MD"
 global.location = "Kenya"
